309054021f471d29c8ee91f66dee33ff91b6a1db
L <60981745+pankourgiou@users.noreply.github.com>
2024-11-05 21:51:07 +0200

    Create Stats.py

---
+++ Stats.py
@@ -0,0 +1,99 @@
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
+
+from matplotlib import pyplot
+import numpy
+
+pyplot.figure()
+pyplot.plot(numpy.random.rand(10))
+pyplot.show()
